bbf1646542b3ea1a17a536c593e89878a1ab3ce4
Ziv Avivi <137340642+Av1zi@users.noreply.github.com>
2026-07-22 23:08:22 +0300

    Update the player model to use our assets

--- new-game-project/Scripts/player.gd
+++ new-game-project/Scripts/player.gd
@@ -12,17 +12,17 @@ var push_dir: Vector2 = Vector2(0, 0)
 var push_strength: float = 0.0
 var push_timer: float = 0.0
 
-# Reference
-@onready var body_lr: Polygon2D = $BodyLR
-@onready var body_rotate: Polygon2D = $BodyRotate
+# Reference (Fixed Paths to match Scene Tree)
+@onready var body_lr: Node2D = $character
+@onready var body_rotate: Node2D = $gun
 @onready var body_lr_player: AnimationPlayer = $BodyLRPlayer
 @onready var body_rotete_player: AnimationPlayer = $BodyRotatePlayer
 @onready var move_trail_effect: GPUParticles2D = $MovementTrailEffect
 @onready var bullet_scene = preload("res://scenes/bullet.tscn")
-@onready var bullet_spawn_pos: Node2D = $BodyRotate/BulletSpawnPoint
+@onready var bullet_spawn_pos: Node2D = $gun/BulletSpawnPoint
 @onready var shot_timer: Timer = $ShotTimer
-@onready var shot_effect: GPUParticles2D = $BodyRotate/ShootingEffect
-@onready var body_lr_collider: CollisionPolygon2D = $CollisionBodyLR
+@onready var shot_effect: GPUParticles2D = $gun/ShootingEffect
+@onready var body_lr_collider: CollisionShape2D = $CollisionBodyLR#Changed to CollisionShape2D/Polygon2D based on standard Godot nodes
 @onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _ready():
@@ -31,6 +31,7 @@ func _ready():
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO # The player's movement vector.
+	
 	# Movement input
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -40,20 +41,26 @@ func _physics_process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+		
 	# Shot input
 	if Input.is_action_pressed("shot") and not is_shot_cd:
 		shoot()
 		is_shot_cd = true
 		shot_timer.start(0.2)
+		
 	# Normalize velocity if move along x and y together
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		move_trail_effect.emitting = true # Play movement trail effect
+		
 	# Handle body_lr
 	update_body_lr()
-	# Handle push
-	push_back(delta)
-	# Limit the player movement, add your character scale if needed
+	
+	# Handle aim toward mouse every frame
+	update_body_rotate(get_global_mouse_position())
+	
+	
+	# Limit the player movement
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	move_and_slide()
@@ -69,35 +76,20 @@ func setup(pos: Vector2):
 func update_body_lr():
 	if not lr_flag:
 		return
-	# Play body animation
+	# Play animations based on movement, but let the mouse handle rotation
 	if velocity.length() > 0:
-		# Move up / down
-		if lr:
-			body_lr_player.play("MoveR")
-		else:
-			body_lr_player.play("MoveL")
-		# Move left / right
-		if velocity.x > 0:
-			body_lr_player.play("MoveR")
-			body_lr_collider.scale.x = -1
-			lr = true
-		elif velocity.x < 0:
-			body_lr_player.play("MoveL")
-			body_lr_collider.scale.x = 1
-			lr = false
+		body_lr_player.play("MoveR")
 	else:
-		# Idle
-		if lr:
-			body_lr_player.play("IdleR")
-		else:
-			body_lr_player.play("IdleL")
+		body_lr_player.play("IdleR")
 
 func update_body_rotate(mouse_pos: Vector2):
 	if not rotate_flag:
 		return
-	# Rotate with mouse
-	body_rotate.look_at(mouse_pos)
-	aim_pos = mouse_pos.normalized()
+	var dir_to_mouse = (mouse_pos - global_position)
+	body_rotate.rotation = dir_to_mouse.angle() + PI/2
+	body_lr.rotation = dir_to_mouse.angle() + PI/2
+	
+	aim_pos = dir_to_mouse.normalized()
 
 func shoot():
 	body_rotete_player.play("Shot")
@@ -113,13 +105,6 @@ func set_push(dir: Vector2, strength: float, timer: float):
 	push_dir = dir
 	push_strength = strength
 	push_timer = timer
-
-func push_back(delta: float):
-	if push_timer > 0.0:
-		position -= push_dir * push_strength * delta
-		push_timer -= delta
-	else:
-		push_timer = 0.0
 
 func _on_shot_timer_timeout():
 	is_shot_cd = false
